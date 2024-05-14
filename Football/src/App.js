25c24d542c54282333b5c2c2e60a7c98f4e4c06e
Dev Aggarwal <devaggarwal1009@gmail.com>
2024-05-14 19:24:49 +0530

    nav bar added

--- Football/src/App.js
+++ Football/src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
 import { ToastContainer } from 'react-toastify';
+import MainNav from './Components/MainNav';
+import {Routes, Route} from "react-router-dom"
+import HomePage from './Pages/HomePage';
+import ContactPage from './Pages/ContactPage';
+import Outlet1 from './Pages/Outlet1';
+import NotFoundPage from './Pages/NotFoundPage';
+import SecondaryNav from './Components/SecondaryNav';
 
 function App() {
   return (
-    <div>
+    <div className='w-full h-screen bg-[#1E1E1E]'>
+      <MainNav/>
+
+      <SecondaryNav/>
+
+      <Routes>
+        <Route path='/' element={<Outlet1/>}>
+          <Route index element={<HomePage/>}/>
+          <Route path="/contact" element={<ContactPage/>}/>
+          <Route path='*' element={<NotFoundPage/>}/>
+        </Route>
+
+      </Routes>
+
       <ToastContainer/>
     </div>
     
   );
 }
 export default App;
+
---
+++ Football/src/Pages/Outlet1.jsx
@@ -0,0 +1,11 @@
+import {Outlet} from 'react-router-dom'
+
+const Outlet1 = () => {
+    return ( 
+        <div>
+            <Outlet/>
+        </div>
+     );
+}
+ 
+export default Outlet1;
