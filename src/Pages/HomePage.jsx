3e7360ff829dcd24090e87fa3c4ad482dc0389ba
Ayush singla <143862828+Ayushsingla1@users.noreply.github.com>
2024-05-14 23:55:20 +0530

    Delete src directory

--- src/Pages/HomePage.jsx
+++
@@ -1,9 +0,0 @@
-const HomePage = () => {
-    return ( 
-        <div className="text-white">
-            Home Page
-        </div>
-     );
-}
- 
-export default HomePage;
--- src/Pages/NotFoundPage.jsx
+++
@@ -1,9 +0,0 @@
-const NotFoundPage = () => {
-    return ( 
-        <div>
-            Error 404 Not Found
-        </div>
-     );
-}
- 
-export default NotFoundPage;
