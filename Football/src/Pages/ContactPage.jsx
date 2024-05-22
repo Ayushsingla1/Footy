22f98eb270301583c43fb72c740dada80e4ed84d
Ayush singla <143862828+Ayushsingla1@users.noreply.github.com>
2024-05-22 22:11:05 +0530

    contact_commit

--- Football/src/Pages/ContactPage.jsx
+++ Football/src/Pages/ContactPage.jsx
@@ -1,7 +1,47 @@
+import { useState } from "react";
+
 const ContactPage = () => {
+
+    const [info,setinfo] = useState({
+        name : "",
+        email : "",
+        message : ""
+    })
+
+    const dataHandler = (event)=>{
+        const {name , value} = event.target;
+        setinfo((prev)=>({
+            ...prev,
+            [name] : value
+        }))
+    }
+    console.log(info);
     return ( 
-        <div>
-            Contact Page
+        <div className="flex flex-col justify-evenly items-center h-[80vh]">
+            <div className="flex justify-center text-4xl pt-5 text-slate-100">CONTACT US</div>
+            <div className="flex justify-center items-center">
+                <div className="w-[100vw] flex justify-center">
+                    <div className="w-[70vw] h-full">
+                    <form className="flex justify-center flex-col items-center gap-y-20">
+                        <div className="flex justify-between w-full items-center">
+                        <label className="text-2xl text-slate-100" htmlFor="name">NAME :</label>
+                        <input className="h-[70px] w-[977px] rounded-lg text-2xl pl-6 bg-siu"  type="text" placeholder="Enter Your Name" value={info.name} name="name" id="name" onChange={dataHandler}></input>
+                        </div>
+                        <div className="flex justify-between w-full items-center">
+                        <label className="text-2xl text-slate-100 " htmlFor="email">EMAIL :</label>
+                        <input  className="h-[70px] w-[977px] rounded-lg text-2xl pl-6 bg-siu" type="email" placeholder="Enter Your Email" value={info.email} name="email" id="email" onChange={dataHandler}></input>
+                        </div>
+                       <div className="flex justify-between w-full items-center">
+                       <label className="text-2xl text-slate-100 " htmlFor="message">MESSAGE :</label>
+                        <input className="h-[120px] w-[977px] rounded-lg text-2xl pl-6 bg-siu" type="text" placeholder="Enter Your message" value={info.message} name="message" id="message" onChange={dataHandler}></input>
+                       </div>
+                    </form>
+                    <div className="flex w-full justify-end pt-8">
+                    <div className=" flex justify-center self-end w-[240px] h-[60px] text-2xl bg-siu hover:cursor-pointer rounded-md"><button type="submit">SUBMIT</button></div>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
      );
 }
